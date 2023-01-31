d9ca3be823183c5d2b759a94be01eeecd218ca94
bowersjuan <bowersjuan@gmail.com>
2023-01-31 16:33:46 -0500

    update Forms css and App home page logo

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,11 @@
+import logo from "../Assets/snack-app-icon-1.png";
+
 const Home = () => {
   return (
     <div>
       <main className="Home">
         <h1>Welcome!</h1>
+        <img src={logo} alt="logo" />
       </main>
     </div>
   );
