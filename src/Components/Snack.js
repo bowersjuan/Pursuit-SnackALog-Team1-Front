4a0f374de234766b258278aa8bbdb5f53f499643
Jonathan-Gaal <jonathangaal@pursuit.org>
2023-01-31 16:34:06 -0500

    css

--- src/Components/Snack.js
+++ src/Components/Snack.js
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom";
-
+import "./Snack.css";
+import healthyHeartLogo from "../Assets/healthy_heart.png";
+import unhealthyHeartLogo from "../Assets/unhealthy_heart.png";
 const Snack = ({ snack }) => {
   return (
     <div className="snack">
       <Link to={`/snacks/${snack.id}`}>
         <div id="snackDetails">
-          <h3>{snack.name}</h3>
+          {" "}
+          <h3>
+            {snack.name}
+            <img
+              id="heartLogoSnack"
+              src={snack.is_healthy ? healthyHeartLogo : unhealthyHeartLogo}
+              alt="heart"
+            />
+          </h3>{" "}
+          <img id="thumbnail" src={snack.image} alt={`${snack.name}`}></img>
         </div>
       </Link>
     </div>
