d6dddb300d984af4bd1feefba1315a9c0c083699
bowersjuan <bowersjuan@gmail.com>
2023-01-31 12:56:53 -0500

    update and style FourOFour Page

--- src/Pages/FourOFour.js
+++ src/Pages/FourOFour.js
@@ -1,8 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import errorLogo from "../Assets/dalle-juan-404-logo.png";
+import "./FourOFour.css";
+
 const FourOFour = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1 className="header">Sorry, no page found</h1>
+    <div className="modal">
+      <div className="modal-content">
+        <div className="modal-body">
+          <img src={errorLogo} alt="error: not found" width="40%" />
+        </div>
+        <div className="modal-footer">
+          <button onClick={() => navigate("/")}>Close</button>
+        </div>
+      </div>
     </div>
   );
 };
+
 export default FourOFour;
